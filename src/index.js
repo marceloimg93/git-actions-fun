3cb09a4a1921f31dc39ce688f06f3ea64f2ab578
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-12-11 00:59:34 -0300

    start express server get / hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req,res) => {
+    return res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(port, () => { console.log(`server is up!`) })
